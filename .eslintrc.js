8f569926c6ad42593a6e094d127dcbdc75c12c48
Domingo Llanes <info@domingollanes.me>
2022-11-19 09:04:02 +0100

    feat: added some extra config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,18 +7,22 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  extends: ["eslint-config-codely"],
-  plugins: ["editorconfig"],
+  extends: ["eslint-config-codely", "plugin:sonarjs/recommended", "plugin:unicorn/recommended"],
+  plugins: ["sonarjs", "unicorn", "write-good-comments"],
   rules: {
     // Rules override
     "prettier/prettier": ["error", { printWidth: 100, useTabs: false }],
+    // Write good comments rules
+    "write-good-comments/write-good-comments": "warn"
   },
   overrides: [
     {
+      plugins: ["jest-formatting"],
       files: ["tests/*.spec.js"],
       extends: [
         "plugin:jest/recommended",
         "plugin:jest/style",
+        "plugin:jest-formatting/recommended"
       ],
     },
   ],
